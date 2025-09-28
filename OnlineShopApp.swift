0c1f5944ec10743d6976f657defdfa02657a8a3f
seteamsh <seteamsh@gmail.com>
2025-09-28 22:25:42 +0500

    Initial commit

---
+++ OnlineShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineShopApp.swift
+//  OnlineShop
+//
+//  Created by Temirlan Zhumashov on 28.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct OnlineShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
